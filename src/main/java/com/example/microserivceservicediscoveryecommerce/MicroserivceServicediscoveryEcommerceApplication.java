7ac8bfaef09f270acca8e31f38544106c81454b4
kbh0581 <kbh0581@gmail.com>
2022-05-07 22:55:18 +0900

    Initial commit

---
+++ src/main/java/com/example/microserivceservicediscoveryecommerce/MicroserivceServicediscoveryEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserivceservicediscoveryecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserivceServicediscoveryEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserivceServicediscoveryEcommerceApplication.class, args);
+    }
+
+}
